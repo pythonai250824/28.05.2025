2d35988cd7d171603584201f0356d59d7c759a9f
pythonai250824 <pythonai250824@gmail.com>
2025-05-28 20:56:21 +0300

    upload

--- point.py
+++ point.py
@@ -1,6 +1,8 @@
 
 import functools
 import math
+from argparse import ArgumentError
+from typing import overload
 
 
 class Point:
@@ -24,6 +26,13 @@ class Point:
     @y.setter
     def y(self, value):
         self.__y = value
+
+    def distance_from00i(self) -> float:
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+    @staticmethod
+    def distance_from00(p: "Point") -> float:
+        return math.sqrt(p.x ** 2 + p.y ** 2)
 
     def __add__(self, other: "Point, int"):
         if isinstance(other, Point):
@@ -66,7 +75,6 @@ class Point:
     # p1 == (3,3)
     # p1 == 4 like (4,4)
     def __eq__(self, other):
-        print('__eq__')
         # * eq requires hash
         # * when 2 objects are equal their hash value needs to be also equals
         # * p1 == p2 ? True -> the hash value should be the same
@@ -91,8 +99,14 @@ class Point:
     def __gt__(self, other):
         if not isinstance(other, Point):
             raise TypeError(f'Point class does not support > for type {type(other)}')
-        dist_self = math.sqrt(self.x ** 2 + self.y ** 2)
-        dist_other = math.sqrt(other.x ** 2 + other.y ** 2)
+
+        dist_self = self.distance_from00i();
+        dist_other = other.distance_from00i()
+
+        # with static recipe
+        #dist_self = Point.distance_from00(self)
+        #dist_other = Point.distance_from00(other)
+
         return dist_self > dist_other
 
     # >=
@@ -103,18 +117,41 @@ class Point:
 
     # <
     def __lt__(self, other):
-        pass
+        if not isinstance(other, Point):
+            raise TypeError(f'Point class does not support < for type {type(other)}')
+        return not self >= other
 
     # <=
     def __le__(self, other):
-        pass
-
+        if not isinstance(other, Point):
+            raise TypeError(f'Point class does not support < for type {type(other)}')
+        return not self > other
 
     def __repr__(self):
         return f"Point ({self.x:.2f}, {self.y:.2f})"
 
     def __str__(self):
         return f"Point ({self.x:.2f}, {self.y:.2f})"
+
+    # must return int
+    def __len__(self):
+        return round(self.distance_from00i())
+
+    def __getitem__(self, item):
+        match item:
+            case 'x'| 0: return self.x
+            case 'y'| 1: return self.y
+            case _:
+                raise ArgumentError(f"the [ ] does not support {type(item)}")
+
+    def __iter__(self):
+        # yield -- like return but keeps the position
+        x = 1
+        yield x
+        x += 1
+        yield x
+        return  # will break the iter
+
 
     # __del__
 
@@ -155,3 +192,18 @@ print('p3 != p1?', p3 != p1)  # False
 
 print('p1 > p2', p1 > p2)
 print('p1 >= p2', p1 >= p2)
+
+print('p1 < p2', p1 < p2)
+print('p1 <= p2', p1 <= p2)
+
+print(f'len({p1}) = ~{len(p1)}')
+
+print('p1 ==', p1)
+print(f"p1['x'] => {p1['x']}")
+print(f"p1[0] => {p1[0]}")
+print(f"p1['y'] => {p1['y']}")
+print(f"p1[1] => {p1[1]}")
+
+# [3.5, 4.7]
+for value in p1:
+    print(value)  # 3.5 4.7
