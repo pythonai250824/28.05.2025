3bb21f02f17fece2158124ba86a5317458fb0d3f
pythonai250824 <pythonai250824@gmail.com>
2025-05-28 18:17:45 +0300

    Initial commit

---
+++ demo.py
@@ -0,0 +1,45 @@
+# This is a sample Python script.
+from typing import overload
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# @override דורס
+# parent (and class) has this method
+# def __str__()
+#     pass
+
+# not exist in PYTHON
+# 1 overload method
+# def a():
+#     return 1
+# overload method:
+# def a(x):
+#     return x + 1
+
+# operator overloading +
+class Point:
+    pass
+
+p1 = Point()
+p2 = Point()
+
+print(p1)
+[1,2] + [3,4]
+join_list([1,2], [3, 4])
+print(len(p1))
+p1 + p2
+print(p2[0])
+# for x in [1,2,3]:
+# for x in p1:
+p1 != p2
+p1 > p2
+b1 = BankAccount()
+b1 > 0
+b3 = b1 + b2
+
+b3 = create_new_account(b1, b2)
+b3 = b1 + b2
+
